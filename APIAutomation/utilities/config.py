f642eecc147ee4a282292ca9fb897040c7ad6f14
arun <arunkumarnehruks@gmail.com>
2021-10-03 20:03:11 +0530

    Final changes with proper folder structuring and last execution reports

--- APIAutomation/utilities/config.py
+++
@@ -1,7 +0,0 @@
-import configparser
-
-
-def get_config():
-    configure = configparser.ConfigParser()
-    configure.read('utilities/properties.ini')
-    return configure
--- APIAutomation/utilities/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-class Log:
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.INFO)
-    logger.addHandler(handler)
